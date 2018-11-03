03601ae19dedadd225257d9411ef2d6a5d405330
akicho8 <akicho8@gmail.com>
2018-11-03 19:23:36 +0900

    [chore] remove style_article

--- app/controllers/style_articles_controller.rb
+++
@@ -1,2 +0,0 @@
-class StyleArticlesController < CodeArticlesController
-end
--- app/models/style_article.rb
+++
@@ -1,7 +0,0 @@
-require "slim/erb_converter"
-
-class StyleArticle < CodeArticle
-  memory_record_reset [
-    {key: "a1922e353bc21d153bf84fa360ffae39", name: "FlexBoxの使い方", point: ""},
-  ]
-end
